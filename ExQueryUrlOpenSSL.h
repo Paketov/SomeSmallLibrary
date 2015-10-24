62aff17a7c6c6d7661fcfdaddf67f208f5472617
Paketov <shlixterberg@gmail.com>
2015-10-24 05:30:12 +0300

    Update ExQueryUrlOpenSSL.h

--- ExQueryUrlOpenSSL.h
+++ ExQueryUrlOpenSSL.h
@@ -12,12 +12,11 @@
 
 
 #define	__QUERY_URL_OPEN_SSL_FIELDS \
-	struct{\
+struct{\
 	SSL_CTX*		ctx;\
 	SSL*			ssl;\
 	decltype(ERR_get_error()) iError;\
-	bool			isVerClient;\
-	}
+}
 
 
 template<bool = true>
@@ -96,7 +95,6 @@ SSLErrOut:
 	{
 		SSLLastError.ctx = nullptr;
 		SSLLastError.ssl = nullptr;
-		SSLLastError.isVerClient = false;
 		SSLLastError.Clear();
 	}
 
@@ -112,7 +110,7 @@ public:
 
 	~__QUERY_URL_OPEN_SSL()
 	{
-	   	EvntUninitFields();
+		EvntUninitFields();
 	}
 
 	union
@@ -124,7 +122,7 @@ public:
 
 			decltype(ERR_get_error()) operator=(decltype(ERR_get_error()) NewErr)
 			{		
-			   return iError = NewErr;
+				return iError = NewErr;
 			}
 
 			void Set()
@@ -145,51 +143,391 @@ public:
 
 			void Clear()
 			{
-			    iError = 0;
+				iError = 0;
 			}
 		} SSLLastError;
+
+
+		/*
+		Info about remote certificate.
+		*/
+		class REMOTE_CERT
+		{
+		public:
+			union
+			{
+				class
+				{
+					friend REMOTE_CERT;
+					__QUERY_URL_OPEN_SSL_FIELDS;
+				public:
+					char * operator()(char * Buf, size_t Len)
+					{
+						if(ssl == nullptr)
+							return nullptr;
+						X509* Cert = SSL_get_peer_certificate (ssl);
+						if(Cert == nullptr)
+							return nullptr;
+						char * str = X509_NAME_oneline(X509_get_subject_name(Cert), 0, 0);
+						if(str == nullptr)
+						{
+							X509_free(Cert);
+							return nullptr;
+						}
+						strncpy(Buf, str, Len);
+						OPENSSL_free(str);
+						X509_free(Cert);
+						return Buf;
+					}
+
+					operator std::basic_string<char>()
+					{
+						if(ssl == nullptr)
+							return "";
+						X509* Cert = SSL_get_peer_certificate (ssl);
+						if(Cert == nullptr)
+							return "";
+						char * str = X509_NAME_oneline(X509_get_subject_name(Cert), 0, 0);
+						if(str == nullptr)
+						{
+							X509_free(Cert);
+							return "";
+						}
+						std::basic_string<char> Ret = str;
+						OPENSSL_free(str);
+						X509_free(Cert);
+						return Ret;
+					}
+				} SubjectName;
+
+				class
+				{
+					__QUERY_URL_OPEN_SSL_FIELDS;
+				public:
+
+					char * operator()(char * Buf, size_t Len)
+					{
+						if(ssl == nullptr)
+							return nullptr;
+						X509* Cert = SSL_get_peer_certificate (ssl);
+						if(Cert == nullptr)
+							return nullptr;
+						char * str = X509_NAME_oneline(X509_get_issuer_name(Cert), 0, 0);
+						if(str == nullptr)
+						{
+							X509_free(Cert);
+							return nullptr;
+						}
+						strncpy(Buf, str, Len);
+						OPENSSL_free(str);
+						X509_free(Cert);
+						return Buf;
+					}
+
+					operator std::basic_string<char>()
+					{
+						if(ssl == nullptr)
+							return "";
+						X509* Cert = SSL_get_peer_certificate (ssl);
+						if(Cert == nullptr)
+							return "";
+						char * str = X509_NAME_oneline(X509_get_issuer_name(Cert), 0, 0);
+						if(str == nullptr)
+						{
+							X509_free(Cert);
+							return "";
+						}
+						std::basic_string<char> Ret = str;
+						OPENSSL_free(str);
+						X509_free(Cert);
+						return Ret;
+					}
+				} IssuerName;
+
+				class
+				{
+					__QUERY_URL_OPEN_SSL_FIELDS;
+				public:
+					inline operator bool()
+					{
+						if(ssl == nullptr)
+							return false;
+						X509* Cert = SSL_get_peer_certificate (ssl);
+						if(Cert != nullptr)
+						{
+							X509_free(Cert);
+							return true;
+						}
+						return false;
+					}
+				} IsHave;
+			};
+
+			inline operator X509*()
+			{
+				return SSL_get_peer_certificate(SubjectName.ssl);
+			}
+
+			static void Free(X509* Cert)
+			{
+				X509_free(Cert);
+			}
+		} RemoteHostCertificate;
+
+
+		/*
+		Info about local certificate.
+		*/
+		class LOCAL_CERT
+		{
+		public:
+			union
+			{
+				class
+				{
+					friend LOCAL_CERT;
+					__QUERY_URL_OPEN_SSL_FIELDS;
+				public:
+					char * operator()(char * Buf, size_t Len)
+					{
+						if(ssl == nullptr)
+							return nullptr;
+						X509* Cert = SSL_get_certificate(ssl);
+						if(Cert == nullptr)
+							return nullptr;
+						char * str = X509_NAME_oneline(X509_get_subject_name(Cert), 0, 0);
+						if(str == nullptr)
+						{
+							X509_free(Cert);
+							return nullptr;
+						}
+						strncpy(Buf, str, Len);
+						OPENSSL_free(str);
+						X509_free(Cert);
+						return Buf;
+					}
+
+					operator std::basic_string<char>()
+					{
+						if(ssl == nullptr)
+							return "";
+						X509* Cert = SSL_get_certificate(ssl);
+						if(Cert == nullptr)
+							return "";
+						char * str = X509_NAME_oneline(X509_get_subject_name(Cert), 0, 0);
+						if(str == nullptr)
+						{
+							X509_free(Cert);
+							return "";
+						}
+						std::basic_string<char> Ret = str;
+						OPENSSL_free(str);
+						X509_free(Cert);
+						return Ret;
+					}
+				} SubjectName;
+
+				class
+				{
+					__QUERY_URL_OPEN_SSL_FIELDS;
+				public:
+
+					char * operator()(char * Buf, size_t Len)
+					{
+						if(ssl == nullptr)
+							return nullptr;
+						X509* Cert = SSL_get_certificate(ssl);
+						if(Cert == nullptr)
+							return nullptr;
+						char * str = X509_NAME_oneline(X509_get_issuer_name(Cert), 0, 0);
+						if(str == nullptr)
+						{
+							X509_free(Cert);
+							return nullptr;
+						}
+						strncpy(Buf, str, Len);
+						OPENSSL_free(str);
+						X509_free(Cert);
+						return Buf;
+					}
+
+					operator std::basic_string<char>()
+					{
+						if(ssl == nullptr)
+							return "";
+						X509* Cert = SSL_get_certificate(ssl);
+						if(Cert == nullptr)
+							return "";
+						char * str = X509_NAME_oneline(X509_get_issuer_name(Cert), 0, 0);
+						if(str == nullptr)
+						{
+							X509_free(Cert);
+							return "";
+						}
+						std::basic_string<char> Ret = str;
+						OPENSSL_free(str);
+						X509_free(Cert);
+						return Ret;
+					}
+				} IssuerName;
+
+				class
+				{
+					__QUERY_URL_OPEN_SSL_FIELDS;
+				public:
+					inline operator bool()
+					{
+						if(ssl == nullptr)
+							return false;
+						X509* Cert = SSL_get_certificate(ssl);
+						if(Cert != nullptr)
+						{
+							X509_free(Cert);
+							return true;
+						}
+						return false;
+					}
+				} IsHave;
+			};
+
+			inline operator X509*()
+			{
+				return SSL_get_certificate(SubjectName.ssl);
+			}
+
+			static void Free(X509* Cert)
+			{
+				X509_free(Cert);
+			}
+		} LocalHostCertificate;
+
 
 		class
 		{
 			__QUERY_URL_OPEN_SSL_FIELDS;
 		public:
-			inline operator bool()
+
+			operator const char*()
 			{
-				return isVerClient;
+				return SSL_get_cipher(ssl);
 			}
-		} IsVerifyClient;
+		} Cipher;
+
+		class 
+		{
+			__QUERY_URL_OPEN_SSL_FIELDS;
+		public:
+			operator long()
+			{
+				if(ctx == nullptr)
+					return -1;
+				return SSL_CTX_get_timeout(ctx);
+			}
+			long operator= (long New)
+			{
+				if(ctx == nullptr)
+					return -1;
+				SSL_CTX_set_timeout(ctx, New);
+				return New;
+			}
+		} LifeTimeout;
+
+		/*
+		Test is verify.
+		*/
+		class
+		{
+			__QUERY_URL_OPEN_SSL_FIELDS;
+		public:
+			operator bool()
+			{
+				if(ctx == nullptr)
+					return false;
+
+				return SSL_CTX_get_verify_mode(ctx) != SSL_VERIFY_NONE;
+			}
+		} IsVerify;
+
+		/*
+		Get and set verify mode.
+		*/
+		class
+		{
+			__QUERY_URL_OPEN_SSL_FIELDS;
+		public:
+			operator int()
+			{
+				if(ctx == nullptr)
+					return SSL_VERIFY_NONE;
+				return SSL_CTX_get_verify_mode(ctx);
+			}
+
+			int operator=(int New)
+			{
+				if(ctx == nullptr)
+					return SSL_VERIFY_NONE;
+				SSL_CTX_set_verify(ctx, New, nullptr);
+				return New;
+			}
+		} VerifyMode;
+
+		/*
+		Get SSL_CTX structure for manualy using.
+		*/
+		class 
+		{
+			__QUERY_URL_OPEN_SSL_FIELDS;
+		public:
+
+			operator SSL_CTX* ()
+			{
+				return ctx;
+			}
+		} CTX;
+
+		/*
+		Get ssl structure for manualy using.
+		*/
+		class
+		{
+			__QUERY_URL_OPEN_SSL_FIELDS;
+		public:
+			operator SSL* ()
+			{
+				return ssl;
+			}		
+		} SSL;
 	};
 
-//
-//	bool SetCertificateInfo(const char * CertFile, int TypeCertFile = SSL_FILETYPE_PEM)
-//	{
-//		if(SSLLastError.ctx != nullptr)
-//			SSL_CTX_free(SSLLastError.ctx);
-//
-//
-//		SSLLastError.ctx = SSL_CTX_new(SSLv23_client_method());
-//		if(SSLLastError.ctx == nullptr)
-//		{
-//			QUERY_URL::SetLastErr(EFAULT);
-//			return false;
-//		}
-//
-//		if(SSL_CTX_use_certificate_file(SSLLastError.ctx, CertFile, TypeCertFile) <= 0)
-//		{
-//lblErrOut:
-//			SSL_CTX_free(SSLLastError.ctx);
-//			QUERY_URL::SetLastErr(EFAULT);
-//			return false;
-//		}
-//		return true;
-//	}
+	/*
+	Set special version of ssl.
+	*/
+	bool InitCTXVersion(const SSL_METHOD* MethodSSL = SSLv23_server_method())
+	{
+		EvntBeforeClose();
+		if(SSLLastError.ctx != nullptr)
+			SSL_CTX_free(SSLLastError.ctx);
+		SSLLastError.ctx = SSL_CTX_new(MethodSSL);
+		if(SSLLastError.ctx == nullptr)
+		{
+			SSLLastError.Set();
+			QUERY_URL::SetLastErr(EFAULT);
+			return false;
+		}
+		return true;
+	}
 
-	bool InitSSLForBind
-	(
+
+	/*
+	Set certificate info. Use for creating server.
+	*/
+	bool SetLocalCertificate
+	(	
 		const char * CertFile, 
 		const char * PrivateKeyFile, 
 		int TypeCertFile = SSL_FILETYPE_PEM, 
-		int TypeKeyFile = SSL_FILETYPE_PEM, 
+		int TypeKeyFile = SSL_FILETYPE_PEM,
+		const SSL_METHOD* MethodSSL = SSLv23_server_method(),
 		bool IsVerifyClient = false,
 		const char * CAFile = nullptr,
 		const char * CAPath = nullptr,
@@ -197,16 +535,8 @@ public:
 		int VerifyDepth = 1
 	)
 	{
-		EvntBeforeClose();
-
-		SSLLastError.ctx = SSL_CTX_new(SSLv3_method());
-		if(SSLLastError.ctx == nullptr)
-		{
-			SSLLastError.Set();
-			QUERY_URL::SetLastErr(EFAULT);
+		if(!InitCTXVersion(MethodSSL))
 			return false;
-		}
-
 		if(SSL_CTX_use_certificate_file(SSLLastError.ctx, CertFile, TypeCertFile) <= 0)
 		{
 lblErrOut:
@@ -216,7 +546,6 @@ lblErrOut:
 			QUERY_URL::SetLastErr(EFAULT);
 			return false;
 		}
-
 		if(SSL_CTX_use_PrivateKey_file(SSLLastError.ctx, PrivateKeyFile, TypeKeyFile) <= 0)
 			goto lblErrOut;
 
@@ -229,13 +558,13 @@ lblErrOut:
 
 			SSL_CTX_set_verify(SSLLastError.ctx, ModeVerify, nullptr);
 			SSL_CTX_set_verify_depth(SSLLastError.ctx, VerifyDepth);
-			SSLLastError.isVerClient = true;
-		}else
-		{
-			SSLLastError.isVerClient = false;
 		}
 	}
 
+
+	/*
+	Accepting client over ssl.
+	*/
 	bool AcceptClient(__QUERY_URL_OPEN_SSL & DestCoonection)
 	{
 		DestCoonection.Close();
@@ -247,7 +576,6 @@ lblErrOut:
 			SSLLastError.Set();
 			return false;
 		}
-
 		if(SSL_set_fd(DestCoonection.SSLLastError.ssl, DestCoonection.Descriptor) == 0)
 			goto lblErrOut;
 		//https://www.google.ru/?gws_rd=ssl#q=SSL_accept+returned+0
@@ -262,6 +590,7 @@ lblErrOut:
 lblErrOut:
 			SSLLastError.Set();
 lblErrOut2:
+			DestCoonection.QUERY_URL::ShutdownSendRecive();
 			DestCoonection.QUERY_URL::Close();
 			return false;		
 		}
@@ -270,10 +599,13 @@ lblErrOut2:
 
 	virtual int Send(const void * QueryBuf, size_t SizeBuf, int Flags = 0)
 	{
+		if(SSLLastError.ssl == nullptr)
+			goto lblErr;
 		int WritenSize;
 		if((WritenSize = SSL_write(SSLLastError.ssl, QueryBuf, SizeBuf)) < 0)
 		{
 			SSLLastError.Set();
+lblErr:
 			QUERY_URL::SetLastErr(EFAULT);
 			return -1;
 		}
@@ -282,10 +614,13 @@ lblErrOut2:
 
 	virtual int Recive(void * Buf, size_t SizeBuf, int Flags = 0)
 	{
+		if(SSLLastError.ssl == nullptr)
+			goto lblErr;
 		int ReadedSize;
 		if((ReadedSize = SSL_read(SSLLastError.ssl, Buf, SizeBuf)) < 0)
 		{
 			SSLLastError.Set();
+lblErr:
 			QUERY_URL::SetLastErr(EFAULT);
 			return -1;
 		}
@@ -294,6 +629,8 @@ lblErrOut2:
 
 	virtual int Recive(std::basic_string<char> & StrBuf, int Flags = 0)
 	{
+		if(SSLLastError.ssl == nullptr)
+			goto lblErr;
 		char * Buf;
 		unsigned CurSize = 0, CountBytesInBuff = 0;
 		CountBytesInBuff = SSL_pending(SSLLastError.ssl);
@@ -307,6 +644,7 @@ lblErrOut2:
 			if(ReadedSize < 0)
 			{
 				SSLLastError.Set();
+lblErr:
 				QUERY_URL::SetLastErr(EFAULT);
 				return -1;
 			}else if(ReadedSize == 0)
